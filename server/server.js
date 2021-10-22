aad8f120543e47b2840ffd218d1b00b8a0476b9b
tumikole <ramollosamuel2@gmail.com>
2021-10-22 08:24:03 +0200

    modal form changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+require("dotenv/config");
+//imports Routes
+const postRoutes = require("./routes/applicant");
+//Routes
 
-require("dotenv/config")
 
-mongoose.connect(process.env.MONGO_URI,()=> console.log("DB is connected..."))
+mongoose.connect(process.env.MONGO_URI,() => {
+    console.log("DB is connected...")
+})
+app.use(postRoutes)
 const port = 5000
 
 app.listen(port, ()=> console.log(`Server is running on port 5000...`))
