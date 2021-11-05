6afecff02a1aca304da223200f8f5eb37455ce8a
tumikole <ramollosamuel2@gmail.com>
2021-11-05 14:22:30 +0200

    css

--- contact.js
+++ contact.js
@@ -1,36 +1,41 @@
 const submitMessage = () => {
-    const firstname = document.getElementById('firstname').value
-    const lastname = document.getElementById('lastname').value
-    const email = document.getElementById('email').value
-    const message = document.getElementById('message').value
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
- 
-  if(firstname && lastname && email && message !== ''){
+  if (firstname && lastname && email && message !== "") {
     fetch("https://formspree.io/f/mdoyvlyg", {
-        method: 'POST',
-        body: JSON.stringify({Firstname : firstname, Lastname: lastname, Email: email, Message: message}),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        
+      method: "POST",
+      body: JSON.stringify({
+        Firstname: firstname,
+        Lastname: lastname,
+        Email: email,
+        Message: message,
+      }),
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
         swal.fire("We'll get back to you as soon as we receive your request");
-      }).catch(error => {
+      })
+      .catch((error) => {
         swal.fire("error");
       });
     //   firstname.value= ""
-} else{
+  } else {
     Swal.fire({
-        title: 'Please fill all required fields.',
-        width: 600,
-        padding: '3em',
-        // background: '#fff url(./icon/linkedin-in-brands.svg)',
-        backdrop: `
+      title: "Please fill all required fields.",
+      width: 600,
+      padding: "3em",
+      // background: '#fff url(./icon/linkedin-in-brands.svg)',
+      backdrop: `
           rgba(0,0,123,0.4)
           url("/img/warning.png")
           left top
           no-repeat
-        `
-      })}
+        `,
+    });
   }
-
+};
--- script.js
+++ script.js
@@ -1,41 +1,52 @@
 const submitForm = () => {
-    const name = document.getElementById('firstname').value
-    const lastname = document.getElementById('lastname').value
-    const contact = document.getElementById('contact').value
-    const id = document.getElementById('id').value
-    const email = document.getElementById('email').value
-    const address = document.getElementById('address').value
+  const name = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const contact = document.getElementById("contact").value;
+  const id = document.getElementById("id").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
 
- 
-  if(name && lastname && contact && id && email && address !== ''){
+  if (name && lastname && contact && id && email && address !== "") {
     fetch("https://formspree.io/f/xeqvwzvj", {
-        method: 'POST',
-        body: JSON.stringify({Firstname : name, Lastname: lastname, Id: id, Contact: contact, Email: email, Address: address}),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
+      method: "POST",
+      body: JSON.stringify({
+        Firstname: name,
+        Lastname: lastname,
+        Id: id,
+        Contact: contact,
+        Email: email,
+        Address: address,
+      }),
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
         swal.fire("Succesfully registered");
-      }).catch(error => {
+      })
+      .catch((error) => {
         swal.fire("error");
       });
-} else{
-  Swal.fire({
-    title: 'Please fill all required fields.',
-    width: 600,
-    padding: '3em',
-    // background: '#fff url(./icon/linkedin-in-brands.svg)',
-    backdrop: `
+  } else {
+    Swal.fire({
+      title: "Please fill all required fields.",
+      width: 600,
+      padding: "3em",
+      // background: '#fff url(./icon/linkedin-in-brands.svg)',
+      backdrop: `
       rgba(0,0,123,0.4)
       url("")
       left top
       no-repeat
-    `
-  })}
-}
-  
+    `,
+    });
+  }
+};
+
 var mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -47,7 +58,6 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
- 
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -55,17 +65,17 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
 function openForm() {
